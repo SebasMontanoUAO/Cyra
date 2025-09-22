9e1f922d0194ed8bb5a54cacaf759a7b3839f933
chanclaxx <132369542+chanclaxx@users.noreply.github.com>
2025-09-22 14:55:32 -0500

    interfaces

---
+++ Repositories/IProductoCategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿using Cyra.Data;
+
+namespace Cyra.Repositories
+{
+    public interface IProductoCategoriaRepository
+    {
+        Task<ProductoCategoria?> GetByIdAsync(long idProducto, long idCategoria);
+        Task<IEnumerable<ProductoCategoria>> GetByProductoAsync(long idProducto);
+        Task<IEnumerable<ProductoCategoria>> GetByCategoriaAsync(long idCategoria);
+        Task<IEnumerable<ProductoCategoria>> GetAllAsync();
+        Task<ProductoCategoria> AddAsync(ProductoCategoria productoCategoria);
+        Task<bool> DeleteAsync(long idProducto, long idCategoria);
+        Task<bool> DeleteByProductoAsync(long idProducto);
+        Task<bool> DeleteByCategoriaAsync(long idCategoria);
+        Task<bool> AddCategoriasToProductoAsync(long idProducto, IEnumerable<long> idsCategorias);
+        Task<bool> RemoveCategoriasFromProductoAsync(long idProducto, IEnumerable<long> idsCategorias);
+        Task<int> GetCountProductosByCategoriaAsync(long idCategoria);
+    }
+}
---
+++ Repositories/IVendedorRepository.cs
@@ -0,0 +1,18 @@
+﻿using Cyra.Data;
+
+namespace Cyra.Repositories
+{
+    public interface IVendedorRepository
+    {
+        Task<Vendedor?> GetByIdAsync(long idUsuario);
+        Task<Vendedor?> GetByNitAsync(string nit);
+        Task<Vendedor?> GetByNombreNegocioAsync(string nombreNegocio);
+        Task<IEnumerable<Vendedor>> GetAllAsync();
+        Task<IEnumerable<Vendedor>> GetWithUsuarioAsync();
+        Task<Vendedor> AddAsync(Vendedor vendedor);
+        Task<Vendedor> UpdateAsync(Vendedor vendedor);
+        Task<bool> DeleteAsync(long idUsuario);
+        Task<bool> NitExistsAsync(string nit);
+        Task<int> GetTotalVendedoresAsync();
+    }
+}
