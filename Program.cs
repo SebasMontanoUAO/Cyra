fa938b39efb3ba817e7402ed70dde97bad1cd69f
SebasMontanoUAO <147752915+SebasMontanoUAO@users.noreply.github.com>
2025-09-26 18:40:38 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Cyra.Services;
 using Cyra.Services.Interfaces;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
