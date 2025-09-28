410c9e8fca1e6efa170e84731720ffe6d6c90802
chanclaxx <132369542+chanclaxx@users.noreply.github.com>
2025-09-28 13:03:46 -0500

    Modificaciones para el login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,6 @@
 ﻿using Cyra.Data;
 using Cyra.Models;
 using Cyra.Services.Interfaces;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
